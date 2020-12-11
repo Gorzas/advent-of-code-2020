08514015cc881c99f31d0a3aa86283148c6e2ea0
Gorzas <joseda87@gmail.com>
2020-12-11 23:54:02 +0100

    Add solutions for day 11

--- src/day-11/solutions.js
+++ src/day-11/solutions.js
@@ -1,12 +1,104 @@
-function solutionOne(data) {
-  return;
+function getSeatState(arr, x, y, adjacent = false, maxPeople = 4) {
+  let state = arr[x][y];
+
+  if ('.' === state) {
+    return state;
+  }
+
+  const dir = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+  const maxOccupiedSeats = 'L' === state ? 1 : maxPeople;
+  let numOccupiedSeats = 0;
+  let pos = 0;
+
+  while (pos < dir.length && numOccupiedSeats < maxOccupiedSeats) {
+    const dx = dir[pos][0];
+    const dy = dir[pos][1];
+    let i = x + dx;
+    let j = y + dy;
+    let seat;
+
+    pos += 1;
+
+    do {
+      if (!arr[i] || !arr[i][j]) {
+        break;
+      }
+      
+      seat = arr[i][j];
+
+      i += dx;
+      j += dy;
+    } while(!adjacent && seat && '.' === seat);
+
+    if ('#' === seat) {
+      numOccupiedSeats += 1;
+    }
+  }
+
+  if ('L' === state && !numOccupiedSeats) {
+    state = '#';
+  } else if ('#' === state && numOccupiedSeats >= maxPeople) {
+    state = 'L';
+  }
+  
+  return state;
+}
+
+function solutionOne(data, adjacent = true, maxPeople = 4) {
+  let isStable = false;
+  let numOccupiedSeats;
+  let seats = [...data];
+  
+  while (!isStable) {
+    let newSeats = [];
+    numOccupiedSeats = 0;
+    isStable = true;
+
+    for (let i = 0; i < seats.length; i += 1) {
+      for (let j = 0; j < seats[i].length; j += 1) {
+        let seat = seats[i][j];
+        let state = getSeatState(seats, i, j, adjacent, maxPeople);
+
+        if (!newSeats[i]) {
+          newSeats[i] = '';
+        }
+
+        newSeats[i] += state;
+
+        // seat has changed
+        if (state !== seat) {
+          isStable = false;
+        }
+
+        // count occupied
+        if ('#' === state) {
+          numOccupiedSeats += 1;
+        }
+      }
+    }
+
+    seats = newSeats;
+  }
+
+  return numOccupiedSeats;
 }
 
 function solutionTwo(data) {
-  return;
+  return solutionOne(data, false, 5);
 }
 
 module.exports = {
+  getSeatState,
+
   solutionOne,
   solutionTwo,
 };
--- src/day-11/solutions.test.js
+++ src/day-11/solutions.test.js
@@ -1,42 +1,114 @@
 const {
+  getSeatState,
+
   solutionOne,
   solutionTwo,
 } = require('./solutions.js');
 
-test('#solutionOne - with a list of joltages, it should return 1-jolt differences multiplied by 3-jolt differences', () => {
+test('#getSeatState - with floor, it should return .', () => {
   const data = [
-    '28',
-    '33',
-    '18',
-    '42',
-    '31',
-    '14',
-    '46',
-    '20',
-    '48',
-    '47',
-    '24',
-    '23',
-    '49',
-    '45',
-    '19',
-    '38',
-    '39',
-    '11',
-    '1',
-    '32',
-    '25',
-    '35',
-    '8',
-    '17',
-    '7',
-    '9',
-    '4',
-    '2',
-    '34',
-    '10',
-    '3',
+    'L.L',
+    'LLL',
+    'L.L',
   ];
 
-  expect(solutionOne(data)).toBe(220);
+  expect(getSeatState(data, 0, 1)).toBe('.');
+  expect(getSeatState(data, 2, 1)).toBe('.');
+});
+
+test('#getSeatState - with empty seat and empty adjacent seats, it should return #', () => {
+  const data = [
+    'L.L',
+    'LLL',
+    'L.L',
+  ];
+
+  expect(getSeatState(data, 0, 0)).toBe('#');
+  expect(getSeatState(data, 1, 1)).toBe('#');
+  expect(getSeatState(data, 2, 2)).toBe('#');
+});
+
+test('#getSeatState - with empty seat and at least one adjacent seat occupied, it should return L', () => {
+  const data = [
+    'L.L',
+    'L#L',
+    'L.L',
+  ];
+
+  expect(getSeatState(data, 0, 0)).toBe('L');
+  expect(getSeatState(data, 1, 0)).toBe('L');
+  expect(getSeatState(data, 2, 2)).toBe('L');
+});
+
+test('#getSeatState - with seat occupied and four or more adjacent seats occupied, it should return L', () => {
+  const data = [
+    '#.#',
+    'L#L',
+    '#.#',
+  ];
+
+  expect(getSeatState(data, 1, 1)).toBe('L');
+});
+
+test('#getSeatState - with seat occupied and less than four adjacent seats occupied, it should return #', () => {
+  const data = [
+    'L.#',
+    'L##',
+    'L.#',
+  ];
+
+  expect(getSeatState(data, 1, 1)).toBe('#');
+  expect(getSeatState(data, 0, 2)).toBe('#');
+  expect(getSeatState(data, 1, 2)).toBe('#');
+});
+
+test('#getSeatState - with seat occupied and less than five adjacent seats occupied, it should return #', () => {
+  const data = [
+    '#.##.##.##',
+    '#######.##',
+    '#.#.#..#..',
+    '####.##.##',
+    '#.##.##.##',
+    '#.#####.##',
+    '..#.#.....',
+    '##########',
+    '#.######.#',
+    '#.#####.##'
+  ];
+
+  expect(getSeatState(data, 1, 0, false, 5)).toBe('#');
+});
+
+test('#solutionOne - with a list of seats, it should return the optimize number of occupied seats', () => {
+  const data = [
+    'L.LL.LL.LL',
+    'LLLLLLL.LL',
+    'L.L.L..L..',
+    'LLLL.LL.LL',
+    'L.LL.LL.LL',
+    'L.LLLLL.LL',
+    '..L.L.....',
+    'LLLLLLLLLL',
+    'L.LLLLLL.L',
+    'L.LLLLL.LL',
+  ];
+
+  expect(solutionOne(data)).toBe(37);
+});
+
+test('#solutionTwo - with a list of seats, it should return the optimize number of occupied seats', () => {
+  const data = [
+    'L.LL.LL.LL',
+    'LLLLLLL.LL',
+    'L.L.L..L..',
+    'LLLL.LL.LL',
+    'L.LL.LL.LL',
+    'L.LLLLL.LL',
+    '..L.L.....',
+    'LLLLLLLLLL',
+    'L.LLLLLL.L',
+    'L.LLLLL.LL',
+  ];
+
+  expect(solutionTwo(data)).toBe(26);
 });
